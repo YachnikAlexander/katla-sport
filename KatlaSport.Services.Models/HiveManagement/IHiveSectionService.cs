a8eff0c08f7db59f65b7241befa217b8c04c78d7
Yachnik Alexander <sansanich230899@gmail.com>
2018-10-13 10:17:02 +0300

    Merge step13. implement methods for adding, editing, and deleting the HiveSection entity. All methods must be asynchronous. Step_13_finish

--- KatlaSport.Services.Models/HiveManagement/IHiveSectionService.cs
+++ KatlaSport.Services.Models/HiveManagement/IHiveSectionService.cs
@@ -29,6 +29,29 @@ namespace KatlaSport.Services.HiveManagement
         Task<List<HiveSectionListItem>> GetHiveSectionsAsync(int hiveId);
 
         /// <summary>
+        /// Creates a new hive section.
+        /// </summary>
+        /// <param name="hiveId">A hive idenfier. </param>
+        /// <param name="createRequest"><see cref="UpdateHiveSectionRequest"/>.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task<HiveSection> CreateHiveSectionAsync(UpdateHiveSectionRequest createRequest);
+
+        /// <summary>
+        /// Updates an existed hive section.
+        /// </summary>
+        /// <param name="hiveSectionId">A hive srction identifier.</param>
+        /// <param name="updateRequest">A <see cref="UpdateHiveSectionRequest"/>.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task<HiveSection> UpdateHiveSectionAsync(int hiveSectionId, UpdateHiveSectionRequest updateRequest);
+
+        /// <summary>
+        /// Deletes an existed hive.
+        /// </summary>
+        /// <param name="hiveSectionId">A hive identifier.</param>
+        /// /// <returns>A <see cref="Task"/>.</returns>
+        Task DeleteHiveAsync(int hiveSectionId);
+
+        /// <summary>
         /// Sets deleted status for a hive section.
         /// </summary>
         /// <param name="hiveSectionId">A hive section identifier.</param>
